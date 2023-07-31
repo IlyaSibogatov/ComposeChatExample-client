93c39a12d08f9d7a360ba0896f9f97224d871ca3
IlyaSibogatov <ilya.sibogatov@gmail.com>
2023-07-31 18:10:39 +0300

    - Adding followers and friend requests to the user profile - Change the screen for friends to the general screen friends/followers/friend requests - Using API methods to request friendship and accept/reject it - Adding strings and constants

--- app/src/main/java/com/example/composechatexample/data/remote/UserServiceImpl.kt
+++ app/src/main/java/com/example/composechatexample/data/remote/UserServiceImpl.kt
@@ -1,21 +1,80 @@
 package com.example.composechatexample.data.remote
 
-import com.example.composechatexample.data.model.UserDTO
-import com.example.composechatexample.domain.model.User
+import com.example.composechatexample.data.model.UserFromId
+import com.example.composechatexample.data.response.DefaultResponse
+import com.example.composechatexample.domain.model.Friend
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
+import io.ktor.client.request.post
+import io.ktor.http.HttpStatusCode
 
 class UserServiceImpl(
     private val client: HttpClient
 ) : UserService {
-    override suspend fun getUserById(uid: String): User? {
+    override suspend fun getUserById(uid: String): UserFromId? {
         return try {
-            client.get<UserDTO>(UserService.EndPont.GetUserById.url) {
+            client.get<UserFromId>(UserService.EndPoint.GetUserById.url) {
                 url.parameters.append("uid", uid)
-            }.toUser()
+            }
         } catch (e: Exception) {
             e.printStackTrace()
             null
+        }
+    }
+
+    override suspend fun friendshipRequest(selfId: String, userId: String): Boolean? {
+        return try {
+            client.post<DefaultResponse>(UserService.EndPoint.AddFriends.url) {
+                url.parameters.append("selfId", selfId)
+                url.parameters.append("userId", userId)
+            }.let {
+                when (it.status) {
+                    HttpStatusCode.OK.value -> {
+                        true
+                    }
+
+                    HttpStatusCode.NoContent.value -> {
+                        false
+                    }
+
+                    else -> {
+                        false
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+
+    override suspend fun getFollowerFriends(uid: String, type: String): List<Friend>? {
+        return try {
+            client.get<List<Friend>>(UserService.EndPoint.GetFollowerFriends.url) {
+                url.parameters.append("uid", uid)
+                url.parameters.append("type", type)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+
+    override suspend fun friendshipAccept(
+        selfId: String,
+        userId: String,
+        action: Boolean
+    ): Boolean {
+        return try {
+            client.post<DefaultResponse>(UserService.EndPoint.FriendshipAction.url) {
+                url.parameters.append("selfId", selfId)
+                url.parameters.append("userId", userId)
+                url.parameters.append("accept", action.toString())
+            }
+            true
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
         }
     }
 }
---
+++ app/src/main/java/com/example/composechatexample/screens/profile/userlist/UsersListViewModel.kt
@@ -0,0 +1,82 @@
+package com.example.composechatexample.screens.profile.userlist
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.composechatexample.data.preferences.PreferencesManager
+import com.example.composechatexample.data.remote.UserService
+import com.example.composechatexample.screens.profile.FriendListUIState
+import com.example.composechatexample.screens.profile.userlist.model.UsersListEvent
+import com.example.composechatexample.utils.Constants
+import com.example.composechatexample.utils.Constants.FRIENDS
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class UsersListViewModel @Inject constructor(
+    private val userService: UserService,
+    private val preferencesManager: PreferencesManager
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(FriendListUIState())
+    val uiState: StateFlow<FriendListUIState> = _uiState
+
+    private val eventChannel = Channel<UsersListEvent>(Channel.BUFFERED)
+    val eventsFlow = eventChannel.receiveAsFlow()
+
+    fun getFollowerFriends(uid: String?, type: String?) {
+        _uiState.value = uiState.value.copy(
+            uid = uid ?: preferencesManager.uuid!!,
+            screenType = type ?: FRIENDS,
+            loadingStatus = true,
+        )
+        viewModelScope.launch {
+            userService.getFollowerFriends(uiState.value.uid, uiState.value.screenType)?.let {
+                _uiState.value = uiState.value.copy(
+                    usersList = it,
+                    loadingStatus = false,
+                )
+            }
+        }
+    }
+
+    fun friendshipsAccept(userId: String, accept: Boolean) {
+        viewModelScope.launch {
+            _uiState.value = uiState.value.copy(
+                loadingStatus = true
+            )
+            userService.friendshipAccept(preferencesManager.uuid!!, userId, accept).let {
+                if (it) {
+                    val list = uiState.value.usersList.toMutableList()
+                    list.remove(list.find { user -> user.id == userId })
+                    _uiState.value = uiState.value.copy(
+                        usersList = list
+                    )
+                }
+                _uiState.value = uiState.value.copy(
+                    loadingStatus = false
+                )
+            }
+        }
+    }
+
+    fun openProfile(uid: String) {
+        if (uid == preferencesManager.uuid)
+            sendEvent(UsersListEvent.NavigateTo(Constants.PROFILE_ROUTE))
+        else
+            sendEvent(
+                UsersListEvent.NavigateTo(
+                    "${Constants.PROFILE_ROUTE}/${Constants.USER_UID}${uid}"
+                )
+            )
+    }
+
+    private fun sendEvent(event: UsersListEvent) {
+        viewModelScope.launch {
+            eventChannel.send(event)
+        }
+    }
+}
