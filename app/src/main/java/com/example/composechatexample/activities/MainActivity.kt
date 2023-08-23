79ad4c8e2a1a7456e4f90603d8ab9939eeee60ed
Petr Grebnev <95710980+PetrGrebnev@users.noreply.github.com>
2023-08-23 21:42:29 +0300

    Feature/animate search field (#24)
    * Feature

--- app/src/main/java/com/example/composechatexample/activities/MainActivity.kt
+++ app/src/main/java/com/example/composechatexample/activities/MainActivity.kt
@@ -1,18 +1,22 @@
 package com.example.composechatexample.activities
 
 import android.os.Bundle
+import android.os.LocaleList
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalConfiguration
 import com.example.composechatexample.navigation.MainEntryPoint
 import com.example.composechatexample.ui.theme.ComposeChatExampleTheme
+import com.example.composechatexample.ui.theme.configurationState
 import com.example.composechatexample.ui.theme.themeState
 import com.example.composechatexample.utils.Ext.getLocale
 import com.example.composechatexample.utils.Ext.setLanguage
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Locale
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -26,8 +30,20 @@ class MainActivity : ComponentActivity() {
             themeState = remember {
                 mutableStateOf(viewModel.getTheme())
             }
-            ComposeChatExampleTheme {
-                MainEntryPoint()
+            val langState = remember {
+                mutableStateOf(Locale(viewModel.getLocale(getLocale(this))))
+            }
+            val config = LocalConfiguration.current
+            config.setLocales(LocaleList(langState.value))
+            configurationState = remember {
+                mutableStateOf(config)
+            }
+            CompositionLocalProvider(
+                values = arrayOf(LocalConfiguration provides configurationState.value)
+            ) {
+                ComposeChatExampleTheme {
+                    MainEntryPoint()
+                }
             }
         }
     }
