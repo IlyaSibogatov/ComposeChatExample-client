662459a44d430013b6bd6121fecef090c72dfcbf
IlyaSibogatov <ilya.sibogatov@gmail.com>
2023-08-25 14:25:32 +0300

    Added: - Username validation - Password validation - Display validation errors - Validator class
    - Search chat icon for clear search request
    - Search icon moved

--- app/src/main/java/com/example/composechatexample/screens/chat/chatlist/ChatListScreen.kt
+++ app/src/main/java/com/example/composechatexample/screens/chat/chatlist/ChatListScreen.kt
@@ -3,17 +3,10 @@ package com.example.composechatexample.screens.chat.chatlist
 import android.widget.Toast
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.expandHorizontally
-import androidx.compose.animation.fadeIn
 import androidx.compose.animation.shrinkHorizontally
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.focusable
-import androidx.compose.foundation.interaction.Interaction
-import androidx.compose.foundation.interaction.InteractionSource
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -29,7 +22,6 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
@@ -46,9 +38,6 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.focus.focusTarget
-import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.focus.onFocusEvent
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
@@ -84,7 +73,7 @@ import com.example.composechatexample.utils.ResponseStatus
 import com.example.composechatexample.utils.Type
 import kotlinx.coroutines.flow.collectLatest
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun ChatListScreen(
     navController: NavHostController,
@@ -119,7 +108,7 @@ fun ChatListScreen(
                         start.linkTo(parent.start)
                         end.linkTo(parent.end)
                     },
-                horizontalArrangement = Arrangement.Center
+                horizontalArrangement = Arrangement.End
             ) {
                 val animState = remember {
                     mutableStateOf(false)
@@ -284,6 +273,15 @@ fun SearchField(
                 keyboardController?.hide()
                 imVisible(true)
             }),
+            trailingIcon = {
+                CustomIconButton(
+                    imageId = R.drawable.ic_clear,
+                    onClick = {
+                        if (value.isNotEmpty())
+                            onValueChange("")
+                    }
+                )
+            },
         )
     }
 }
