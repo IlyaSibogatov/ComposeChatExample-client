d89888880f3507aa8ac56fd70054609cd745c2e6
IlyaSibogatov <ilya.sibogatov@gmail.com>
2023-09-21 12:51:19 +0300

    Added: - Use flow events for update notification screen when received new notification; - New state - Empty data screen, for ChatsListScreen & NotificationsScreen; - Create view for displaying error & empty data screens; - Implemented change "notification" setting. Updated: - ChatListScreen & NotifiactionsScreen - String resources.

--- app/src/main/java/com/example/composechatexample/utils/firebase/FirebaseService.kt
+++ app/src/main/java/com/example/composechatexample/utils/firebase/FirebaseService.kt
@@ -1,4 +1,4 @@
-package com.example.composechatexample.utils
+package com.example.composechatexample.utils.firebase
 
 import android.Manifest
 import android.annotation.SuppressLint
@@ -17,11 +17,18 @@ import androidx.core.app.NotificationManagerCompat
 import com.example.composechatexample.R
 import com.example.composechatexample.activities.NotificationActivity
 import com.example.composechatexample.data.model.NotificationModel
+import com.example.composechatexample.data.model.NotificationType
+import com.example.composechatexample.data.model.UserNotification
 import com.example.composechatexample.data.preferences.PreferencesManager
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.google.gson.Gson
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import kotlin.random.Random
 
@@ -37,6 +44,12 @@ class FirebaseService() : FirebaseMessagingService() {
     private var messageData: NotificationModel? = null
     private var notificationId: Int? = null
     private var gson = Gson()
+
+    private fun sendEvent(event: NotificationEvent) {
+        GlobalScope.launch {
+            eventChannel.send(event)
+        }
+    }
 
     override fun onNewToken(token: String) {
         this.token = token
@@ -56,22 +69,34 @@ class FirebaseService() : FirebaseMessagingService() {
 
     override fun onMessageReceived(message: RemoteMessage) {
         if (message.data.isNotEmpty()) {
-            messageData = NotificationModel(
-                title = message.data["title"].toString(),
-                text = message.data["body"].toString(),
-            )
-            createNotification(messageData!!)
+            if (preferencesManager.notificationScreenOpen) {
+                sendEvent(
+                    NotificationEvent.AddNotification(
+                        UserNotification(
+                            id = message.data["id"].toString(),
+                            type = when (message.data["type"].toString()) {
+                                NotificationType.REQUEST_FRIENDSHIP.value -> NotificationType.REQUEST_FRIENDSHIP
+                                NotificationType.ACCEPTED_FRIENDSHIP.value -> NotificationType.ACCEPTED_FRIENDSHIP
+                                NotificationType.DECLINED_FRIENDSHIP.value -> NotificationType.DECLINED_FRIENDSHIP
+                                NotificationType.USER_ACCEPT_FRIENDSHIP.value -> NotificationType.USER_ACCEPT_FRIENDSHIP
+                                NotificationType.USER_DECLINED_FRIENDSHIP.value -> NotificationType.USER_DECLINED_FRIENDSHIP
+                                else -> NotificationType.REQUEST_FRIENDSHIP
+                            },
+                            senderId = message.data["senderId"].toString(),
+                            senderName = message.data["senderName"].toString(),
+                        )
+                    )
+                )
+            } else {
+                if (preferencesManager.notification) {
+                    messageData = NotificationModel(
+                        title = message.data["title"].toString(),
+                        text = message.data["body"].toString(),
+                    )
+                    createNotification(messageData!!)
+                }
+            }
         }
-//        message.notification?.let {
-//            message.data.let { data ->
-//                messageData = NotificationModel(
-//                    title = gson.fromJson(data["title"].toString(), String::class.java),
-//                    text = gson.fromJson(data["text"].toString(), String::class.java),
-//                    sender = gson.fromJson(data["sender"].toString(), String::class.java)
-//                )
-//            }
-//            createNotification(it.title, it.body)
-//        }
         super.onMessageReceived(message)
     }
 
@@ -150,5 +175,9 @@ class FirebaseService() : FirebaseMessagingService() {
         private const val DURATION = 1000L
         private const val MESSAGE_DATA = "message_data"
         const val DEVICE_TYPE = "android_device"
+
+        private val eventChannel = Channel<NotificationEvent>(Channel.BUFFERED)
+        val eventsFlow = eventChannel.receiveAsFlow()
+        fun subscribeEvent(): Flow<NotificationEvent?> = eventsFlow
     }
 }
